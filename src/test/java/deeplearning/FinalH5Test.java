9f4e15f627635f6f51860aa4472fb408f89ad872
Tobias Steidle <tobias.steidle@softwaredev.de>
2020-03-01 09:43:40 +0100

    feat: update model loading

--- src/test/java/deeplearning/FinalH5Test.java
+++ src/test/java/deeplearning/FinalH5Test.java
@@ -25,28 +25,37 @@ public class FinalH5Test {
 
     @Test
     void name() throws Exception {
-        String fileName = "/Users/stefanostermayr/Documents/MeetupJUG2020DL4J/model.h5";
+        //String fileName = "D:/Deep Learning/NLP-Text-Classification/pretrained/final_news.h5";
+        String fileName = "D:/Deep Learning/NLP-Text-Classification/model.h5";
+
+        //String fileName = "/Users/stefanostermayr/Documents/MeetupJUG2020DL4J/model.h5";
 //        String fileNameSimple = "/Users/stefanostermayr/Documents/MeetupJUG2020DL4J/model.h5";
 
-//        final MultiLayerNetwork network = KerasModelImport.importKerasSequentialModelAndWeights(fileName, false);
-//        final ComputationGraph network = KerasModelImport.importKerasModelAndWeights(fileName,
-//        new int[] {20 } ,
-//                false);
+        //final MultiLayerNetwork network = KerasModelImport.importKerasSequentialModelAndWeights(fileName, false);
+        final ComputationGraph network = KerasModelImport.importKerasModelAndWeights(fileName,false);
 
+        //final MultiLayerNetwork network = KerasModelImport.importKerasSequentialModelAndWeights("D:/model.json", "D:/model_weights.h5", false);
+/*
         KerasModelBuilder builder = new KerasModel().modelBuilder()
+                .modelJsonFilename("D:/model.json")
+                .weightsHdf5Filename("D:/model.h5")
+
+
 //                .weightsHdf5Filename()
 //                .modelJsonFilename("")
-                .inputShape(new int[] {-1, 9920 })
-                .modelHdf5Filename(fileName)
+                .inputShape(new int[] {20 })
+                //.modelHdf5Filename(fileName)
 
-//                .dimOrder(KerasLayer.DimOrder.TENSORFLOW)
+               // .dimOrder(KerasLayer.DimOrder.THEANO)
                 .enforceTrainingConfig(false);
-        System.out.println("shape " + Arrays.toString(builder.getInputShape()));
 
+
+        System.out.println("shape " + Arrays.toString(builder.getInputShape()));
+*/
         // reshape input (none, 20, 200), output (None, 1, 20, 200)
         //
 
-        final ComputationGraph network = builder.buildModel().getComputationGraph();
+        //final ComputationGraph network = builder.buildModel().getComputationGraph();
 
 
         //        final MultiLayerNetwork network = KerasModelImport.importKerasSequentialModelAndWeights(fileName);
@@ -59,10 +68,10 @@ public class FinalH5Test {
 
         // Invalid input: EmbeddingSequenceLayer expects either rank 2 input of shape [minibatch,seqLength] or rank 3
         // input of shape [minibatch,1,seqLength]. Got rank 3 input of shape [0, 0, 0]
-        final INDArray[] output = network.output(input);
+        final INDArray [] output = network.output(input);
 
         System.out.println("output " +output[0]);
 
-
+//['POLITICS', 'WELLNESS', 'ENTERTAINMENT', 'TRAVEL', 'STYLE & BEAUTY', 'PARENTING', 'HEALTHY LIVING', 'QUEER VOICES', 'FOOD & DRINK', 'BUSINESS']
     }
 }
